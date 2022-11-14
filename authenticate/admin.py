ac19effaa14aa1588e0b80e90fd1cd4e92edfd7c
Nirali Kanwani <98006740+riyab1110@users.noreply.github.com>
2022-11-13 23:15:48 -0500

    add new image Frontend

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import UserData, Tasks, PhotoData
+from .models import UserData, Tasks, PhotoData, Tags, Phototag
 # Register your models here.
 
 admin.site.register(UserData)
 admin.site.register(Tasks)
 admin.site.register(PhotoData)
+admin.site.register(Phototag)
+admin.site.register(Tags)
